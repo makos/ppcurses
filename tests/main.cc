c64a756fa9c5d7a35d1388ead06cee734137d6f7
Mateusz Makowski <matmakos@gmail.com>
2018-09-07 15:50:16 +0200

    test fix

--- tests/main.cc
+++ tests/main.cc
@@ -6,7 +6,7 @@
 int main() {
   ppc::Application app;
   ppc::Window &my_win = app.NewWindow(ppc::Point(7, 7), ppc::Point(10, 10));
-  ppc::Window &my_win2 = app.NewWindow(ppc::Point(11, 10), ppc::Point(3, 3));
+  ppc::Window &my_win2 = app.NewWindow(ppc::Point(19, 10), ppc::Point(5, 5));
   app.Draw();
   char ch;
   while ((ch = app.GetKeyEvents()) != 'q') {
