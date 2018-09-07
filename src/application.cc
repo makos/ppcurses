18b42b67e340a5b0fb1fda7e21b9476dbe48b581
Mateusz Makowski <matmakos@gmail.com>
2018-09-07 22:23:22 +0200

    Use shared_ptr for NewWindow

--- src/application.cc
+++ src/application.cc
@@ -11,10 +11,10 @@ Application::Application()
 
 Application::~Application() { endwin(); }
 
-Window &Application::NewWindow(Point pos, Point size) {
-  windows_.push_back(std::make_unique<Window>(pos, size));
+std::shared_ptr<Window> Application::NewWindow(Point pos, Point size) {
+  windows_.push_back(std::make_shared<Window>(pos, size));
   focus_id_ = windows_.size() - 1;
-  return **windows_.end();
+  return windows_[windows_.size() - 1];
 }
 
 void Application::Draw() {
