6c77d5beae02928c85f8264e074b6567f1e052b3
Mateusz Makowski <matmakos@gmail.com>
2018-09-07 08:40:36 +0200

    Library structure
    Move out main() out from the base src/ tree into tests/ to use for integration and unit testing

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,10 +1,7 @@
-#include "screen/screen.hpp"
 #include "util/point.hpp"
 #include <iostream>
 
 int main() {
-    // Screen::testInt = 2;
-    // std::cout << Screen::testInt << std::endl;
     ppc::point p;
     p.x == 0 ? std::cout << "y" << std::endl : std::cout << "n" << std::endl;
     return 0;
